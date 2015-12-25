ac836af8f91d9ff70728aed3188c1832f646aa1e
Raghav <ragavlura@gmail.com>
2015-12-25 20:30:27 +0530

    Refactor services & added adoption view

---
+++ platforms/android/assets/www/js/controllers/utils.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Blue Cross of India:Utilities Controller
+ * @author Raghav
+ * @description
+ * # Utilities Controller
+ * Contains the controller for utility functions such as history, state changes
+ */
+
+angular
+
+.module('bluecross.controllers')
+
+.controller('utils', function($scope, $location, $ionicHistory){
+
+	$scope.go = function ( path ) {
+	  if( path == 'back') {
+	  	$ionicHistory.goBack();
+	  }
+	  else {
+	  	$location.path( path );
+	  }
+	};
+	
+})
---
+++ platforms/android/assets/www/js/routes.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Blue Cross of India:Routes
+ * @author Raghav
+ * @description
+ * # Routes
+ * Contains the URL and states routes for the Blue Cross of India app
+ */
+
+angular
+
+.module('bluecross')
+
+.config(function($stateProvider, $urlRouterProvider) {
+	
+        $stateProvider
+            .state('index', {
+                url: '/home',
+                templateUrl: 'views/home.html',
+                controller: 'homeController'
+            })
+
+            .state('register', {
+                url: '/register',
+                templateUrl: 'views/register.html',
+                controller: 'caseController'
+            })
+
+            .state('ambulance_assigned', {
+                url: '/ambulance_assigned',
+                templateUrl: 'views/ambulance_assigned.html',
+                controller : 'ambulanceController'
+            })
+
+            .state('ambulance_not_assigned', {
+                url: '/ambulance_not_assigned',
+                templateUrl: 'views/ambulance_not_assigned.html',
+                controller: 'ambulanceController'
+            })
+
+            .state('adoption',{
+                url: '/adoption',
+                templateUrl: 'views/adoption.html'
+            })
+
+            .state('donate',{
+                url: '/donate',
+                templateUrl: 'views/donate.html'
+            })
+
+            .state('about', {
+                url: '/about',
+                templateUrl: 'views/about.html'
+            })
+          
+        $urlRouterProvider.otherwise("/home");
+
+})
+
+
